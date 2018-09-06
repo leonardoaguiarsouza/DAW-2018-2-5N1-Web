9ddf3543e8b43520463cd3db4298d333c9ffec92
201613260113 <201613260113@PC-215712.passofundo.ifsul.local>
2018-09-06 20:57:20 -0300

    Primeiro commit do projeto.

---
+++ src/java/br/edu/ifsul/controle/ControlePais.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.controle;
+
+import br.edu.ifsul.dao.PaisDAO;
+import br.edu.ifsul.modelo.Pais;
+import br.edu.ifsul.util.Util;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author 201613260113
+ */
+@ManagedBean(name = "controlePais")
+@SessionScoped
+public class ControlePais implements Serializable{
+    private PaisDAO dao;
+    private Pais objeto;
+
+    public ControlePais() {
+        dao = new PaisDAO();
+    }
+
+    public String listar(){
+        return "/privado/pais/listar?faces-redirect=true";
+    }
+    
+    public String novo(){
+        objeto = new Pais();
+        return "formulario?faces-redirect=true";
+    }
+    
+    public String salvar(){
+        if(dao.salvar(objeto)){
+            Util.mensagemInformacao(dao.getMensagem());
+            return "listar?faces-redirect=true";
+        } else {
+            Util.mensagemErro(dao.getMensagem());
+            return "formulario?faces-redirect=true";
+        }
+    }
+    
+    public String cancelar(){
+        return "listar?faces-redirect=true";
+    }
+    
+    public String editar(Integer id){
+        try {
+            objeto = dao.localizar(id);
+            return "formulario?faces-redirect=true";
+        } catch (Exception e) {
+            Util.mensagemErro("Erro ao recuperar objeto: " + Util.getMensagemErro(e));
+            return "listar?faces-redirect=true";
+        }
+    }
+    
+    public void remover(Integer id){
+        objeto = dao.localizar(id);
+        if(dao.remover(objeto)){
+            Util.mensagemInformacao(dao.getMensagem());
+        } else {
+            Util.mensagemErro(dao.getMensagem());
+        }
+    }
+    
+    /**
+     * @return the da
+     */
+    public PaisDAO getDao() {
+        return dao;
+    }
+
+    /**
+     * @param da the da to set
+     */
+    public void setDao(PaisDAO dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * @return the objeto
+     */
+    public Pais getObjeto() {
+        return objeto;
+    }
+
+    /**
+     * @param objeto the objeto to set
+     */
+    public void setObjeto(Pais objeto) {
+        this.objeto = objeto;
+    }
+    
+    
+}
